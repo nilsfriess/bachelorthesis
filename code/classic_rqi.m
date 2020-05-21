56c3d81a31baabef4b2761e26d89984fe10c691a
Nils Friess <N.Friess@stud.uni-heidelberg.de>
2020-05-21 22:30:55 +0200

    DONE

--- code/classic_rqi.m
+++ code/classic_rqi.m
@@ -1,14 +1,14 @@
 function [x, sigma, iterations, eigval_iterates, eigvec_iterates, residuals] = classic_rqi(a, x, sigma, tolerance)
-% CLASSIC_RQI   Computes an eigenpair of a using the Rayleigh quotient
-% iteration
+% CLASSIC_RQI   Computes an eigenpair of the matrix 'a' using the 
+% Rayleigh quotient iteration
 %   [x, sigma, iterations, eigval_iterates, eigvec_iterates] = CLASSIC_RQI(a, x, sigma) 
 %   Uses sigma as an initial guess for the eigenvalue and x as an initial 
-%   guess for the eigenvector of the matrix a. If, however, sigma == inf, 
+%   guess for the eigenvector of the matrix a. If sigma == inf, 
 %   the Rayleigh quotient of x w.r.t. a is used for the initial eigval 
-%   guess. The final approximated eigenvector and -value are stored in x
-%   and sigma, repsectively. The results after every iteration are stored 
-%   in the vectors eigval_iterates and eigvec_iterates. The iteration is 
-%   stopped if the residual is less than 1e-9.
+%   guess. The final approximated eigenvector and eigenvalue are stored in 
+%   x and sigma, respectively. The remaining return arguments contain the
+%   number of iterations and the sequences of approximations of the 
+%   eigenvalue, eigenvectors and residuals, respectively.
 %
 %   [x, sigma, iterations, eigval_iterates, eigvec_iterates] = CLASSIC_RQI(a, x, sigma, tol) 
 %   Same as above except that the computation is stopped when the residual 
@@ -17,18 +17,18 @@ function [x, sigma, iterations, eigval_iterates, eigvec_iterates, residuals] = c
     if nargin < 3
         error("Too few arguments");
     elseif nargin < 4
-        % if no tolerance provided, use 10e-9
-        tolerance = 1e-11; 
+        % if no tolerance provided, use 10^(-9)
+        tolerance = 1e-9; 
     end
 
     m = size(a);
-    x = x / norm(x); % normalise the eigenvector guess
+    x = x / norm(x);
     
     if sigma == inf
-       sigma =  x' * a * x;
+       sigma =  x' * a * x; % Compute Rayleigh Quotient
     end
     
-    eigval_iterates = [sigma];  % save the approximations for debugging,
+    eigval_iterates = [sigma];  % Save the approximations for debugging,
     eigvec_iterates = [x];      % plotting or convergence analysis
     
     res = norm((a - sigma*speye(m))*x);
